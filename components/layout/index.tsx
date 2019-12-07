4afb04317fb27b3084b67f1a2e59eabef66f5aad
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-07 15:07:45 +0800

    feat:添加公共layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
 import Meta from './meta';
+import Header from './header';
+import { UserInfo } from 'types';
+import Snackbar from '@material-ui/core/Snackbar';
 
-export default class Layout extends React.Component {
+export default class Layout extends React.Component<{viewer?: UserInfo, error?: string}, any> {
+    constructor(props: {viewer?: UserInfo, error?: string}) {
+        super(props);
+        this.state = {
+            snakeBar: {
+                show: false,
+                message: ''
+            }
+        };
+        this.snakeClose = this.snakeClose.bind(this);
+        this.snakeExited = this.snakeExited.bind(this);
+    }
+    componentDidMount() {
+        const { error } = this.props;
+        if (error) {
+            this.setState({
+                snakeBar: {
+                    show: true,
+                    message: error
+                }
+            });
+        }
+    }
+    snakeClose() {
+        this.setState({
+            snakeBar: Object.assign({}, this.state.snakeBar, {show: false})
+        });
+    }
+    snakeExited() {
+        this.setState({snakeBar: {message: '', show: false}});
+    }
     render() {
-        const { children } = this.props
+        const { children, viewer } = this.props;
+        const { snakeBar } = this.state;
         return (
             <>
                 <Meta/>
+                <Header viewer={viewer}/>
                 {children}
+                <Snackbar
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    autoHideDuration={1500}
+                    onClose={this.snakeClose}
+                    onExited={this.snakeExited}
+                    open={snakeBar.show}
+                    message={snakeBar.message}
+                />
+                <style jsx global>{`
+                    @media (min-width: 600px) {
+                        .MuiSnackbarContent-root {
+                            min-width: initial!important;
+                        }
+                    }
+                `}</style>
             </>
         )
     }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import App from 'next/app';
 import Layout from 'components/layout/index';
+import fetch, { USER_DATA } from 'api';
 
 export default class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        const pageProps = await Component.getInitialProps(ctx);
+        try {
+            let data = await fetch(USER_DATA);
+            return { pageProps: {...pageProps, viewer: data} };
+        } catch (error) {
+            return { pageProps, error: error.message };
+        }
+    }
     render() {
-        const { Component, pageProps } = this.props
+        const { Component, pageProps, error } = this.props as any;
         return (
-            <Layout>
+            <Layout error={error} viewer={pageProps.viewer}>
                 <Component {...pageProps} />
             </Layout>
         );
