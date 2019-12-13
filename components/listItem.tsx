ffacd9e36aa659d25879f958601d861678e2c83d
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-13 17:03:29 +0800

    feat:首页列表

---
+++ components/listItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import { Typography } from '@material-ui/core';
+import { ArticleType } from 'types';
+
+import 'styles/components/listItem.less';
+
+const ListItem: React.SFC<{ article: ArticleType }> = ({ article }) => {
+    return (
+        <Link href={{pathname: '/article', query: {number: article.issueNumber}}}>
+            <a className="co-list-item">
+                <Typography className="time">{article.publishedAt}</Typography>
+                <Typography className="title"variant="h6">{article.title}</Typography>
+            </a>
+        </Link>
+    );
+};
+export default ListItem;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,15 +1,27 @@
 import { NextPage } from 'next';
 import 'styles/pages/home.less';
+import { Typography } from '@material-ui/core';
+import fetch, { ARTICLE_LIST } from 'api';
+import { ArticleType } from 'types';
+import ListItem from 'components/listItem';
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-    <div>
-        <p>Hello world! - user agent: {userAgent}</p>
-    </div>
-);
+const Home: NextPage<{ articles: ArticleType[] }> = ({ articles }) => {
+    const list = articles.map(article => <ListItem article={article}/>);
+    return (
+        (
+            <div className="pg-home">
+                <div className="head">
+                    <Typography variant="h5">All ArticleTypes</Typography>
+                </div>
+                {list}
+            </div>
+        )
+    );
+};
 
-Home.getInitialProps = async ({req}) => {
-    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-    return { userAgent };
+Home.getInitialProps = async (): Promise<{articles: ArticleType[]}> => {
+    let articles = await fetch(ARTICLE_LIST);
+    return { articles };
 };
 
 export default Home;
