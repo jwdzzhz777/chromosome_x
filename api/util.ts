cf0245cc0015b908221c0c9288d46e0d9949c2b4
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-16 20:24:29 +0800

    feat:优化请求逻辑

--- api/util.ts
+++ api/util.ts
@@ -2,17 +2,23 @@
 import 'isomorphic-unfetch';
 
 export default async (input: RequestInfo, init?: RequestInit): Promise<any> => {
+    init = Object.assign({}, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }, init);
+    let toError = (r: any) => {
+        if (r.ok) return r;
+        else return r.json().then(r => Promise.reject(r));
+    }
     let toJson = (r: any) => r.json();
     let toData = (r: any) => r.data;
-    /** 客户端服务端不同处理 */
-    if (process.browser) {
-        return fetch(input, init).then(toJson).then(toData).catch(err => console.log(err));
-    } else {
-        return fetch(input, init).then(toJson).then(toData).catch(err => {
-            /** 网络错误 */
-            if (err.code === 'ECONNREFUSED') {
-                throw new Error('网络错误');
-            } else return err;
-        });
+    let errDispose = (err: any) => {
+        /** 客户端服务端不同处理 */
+        if (process.browser) console.log(err);
+        /** 网络错误 */
+        else if (err.code === 'ECONNREFUSED') throw new Error('网络错误');
+        else return err;
     }
+    return fetch(input, init).then(toError).then(toJson).then(toData).catch(errDispose);
 };
