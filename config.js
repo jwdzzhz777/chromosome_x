94dbe4861d93677247bba44cbac04d39e85f2229
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-17 17:02:22 +0800

    feat:test production

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
     development: {
         CHROMOSOME_X_URL: 'http://127.0.0.1:7001/api'
+    },
+    production: {
+        CHROMOSOME_X_URL: 'http://127.0.0.1:7001/api'
     }
 };
