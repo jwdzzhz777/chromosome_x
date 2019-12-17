77963fca075a132a961ebab982d8938538e4fe68
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-17 09:58:16 +0800

    feat:update type

--- types.ts
+++ types.ts
@@ -1,3 +1,18 @@
 export interface UserInfo {
     name: string;
+    avatarUrl: string;
+    a_s: string;
+    a_m?: string;
+    a_l?: string;
+    email: string;
+    bio: string;
+    id: string;
+    login: string;
+}
+
+export interface ArticleType {
+    img: string;
+    issueNumber: number;
+    publishedAt: Date;
+    title: string;
 }
