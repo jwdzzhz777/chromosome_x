ee1845bdb731500b3353a9b5608abfed03cf74da
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-23 20:23:10 +0800

    feat:线上配置

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
         CHROMOSOME_X_URL: 'http://127.0.0.1:7001/api'
     },
     production: {
-        CHROMOSOME_X_URL: 'http://127.0.0.1:7001/api'
+        CHROMOSOME_X_URL: 'http://111.229.163.155:7001/api'
     }
 };
