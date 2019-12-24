e46dba460816190fc53a7356e2d04cc151cca20c
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-24 10:39:22 +0800

    feat:优化报错处理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
         const pageProps = Component.getInitialProps? await Component.getInitialProps(ctx) : {};
         let [data, error] = await fetch(USER_DATA);
-        return { pageProps: {...pageProps, viewer: data}, error: error.message || error.msg || '' };
+        return { pageProps: {...pageProps, viewer: data}, error: error && (error.message || error.msg) };
     }
     render() {
         const { Component, pageProps, error } = this.props as any;
