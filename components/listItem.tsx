fa1294b1ba34d02617f1b3a40478a163e53a035c
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-30 18:23:12 +0800

    feat:日期处理

--- components/listItem.tsx
+++ components/listItem.tsx
@@ -9,7 +9,7 @@ const ListItem: React.SFC<{ article: ArticleType }> = ({ article }) => {
     return (
         <Link href={{pathname: '/article', query: {number: article.issueNumber}}}>
             <a className="co-list-item">
-                <Typography className="time">{article.publishedAt}</Typography>
+                <Typography className="time">{new Date(+article.publishedAt).toLocaleString()}</Typography>
                 <Typography className="title"variant="h6">{article.title}</Typography>
             </a>
         </Link>
